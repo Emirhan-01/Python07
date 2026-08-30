720f18a2446562692d21f445b1e942f9befca617
yavrukurt <emirhanbostan352@gmail.com>
2026-08-30 22:35:01 +0300

    ADD: ex0 battle.py

---
+++ battle.py
@@ -0,0 +1,33 @@
+from ex0 import CreatureFactory, FlameFactory, AquaFactory
+
+
+def test_factory(factory: CreatureFactory) -> None:
+    print("Testing factory")
+    base = factory.create_base()
+    print(base.describe())
+    print(base.attack())
+    evolved = factory.create_evolved()
+    print(evolved.describe())
+    print(evolved.attack())
+    print()
+
+
+def battle(factory1: CreatureFactory, factory2: CreatureFactory) -> None:
+    print("Testing battle")
+    creature1 = factory1.create_base()
+    creature2 = factory2.create_base()
+    print(creature1.describe())
+    print("vs.")
+    print(creature2.describe())
+    print("fight!")
+    print(creature1.attack())
+    print(creature2.attack())
+
+
+if __name__ == "__main__":
+    flame_factory = FlameFactory()
+    aqua_factory = AquaFactory()
+
+    test_factory(flame_factory)
+    test_factory(aqua_factory)
+    battle(flame_factory, aqua_factory)
