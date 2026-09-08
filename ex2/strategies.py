3795afc75339388509461f7e9e6ef6e5d22822e4
Emirhan Bostan <embostan@k2m12s01.42kocaeli.com.tr>
2026-09-08 20:21:13 +0300

    strategies.py tamamlandı

--- ex2/strategies.py
+++ ex2/strategies.py
@@ -1,6 +1,7 @@
+from abc import ABC, abstractmethod
 from ex0.creature import Creature
 from ex1.capability import HealCapability, TransformCapability
-from abc import ABC, abstractmethod
+
 
 
 class InvalidStrategyError(Exception):
@@ -8,30 +9,49 @@ class InvalidStrategyError(Exception):
 
 
 class BattleStrategy(ABC):
+    @abstractmethod
     def is_valid(self, creature: Creature) -> bool:
         ...
-        
-    def act(self, creature:Creature) -> str:
+
+    @abstractmethod
+    def act(self, creature:Creature) -> None:
         ...
     
 
 class NormalStrategy(BattleStrategy):
     def is_valid(self, creature) -> bool:
         return True
+    def act(self, creature: Creature) -> None:
+        print(creature.describe())
+        print(creature.attack())
 
 
 class AggressiveStrategy(BattleStrategy):
     def is_valid(self, creature: Creature) -> bool:
         return isinstance(creature, (TransformCapability))
     
-    def act(self, creature: Creature) -> str:
+    def act(self, creature: Creature) -> None:
         if not self.is_valid(creature):
             raise InvalidStrategyError(
                 "Battle error, aborting tournament: Invalid Creature"
                 f" '{creature.name}' for this aggressive strategy")
-
+        assert isinstance(creature, TransformCapability)
+        print(creature.describe())
+        print(creature.transform())
+        print(creature.attack())
+        print(creature.revert())
 
 
 class DefensiveStrategy(BattleStrategy):
-    def is_valid(self, creature):
+    def is_valid(self, creature) -> bool:
         return isinstance(creature, (HealCapability))
+    
+    def act(self, creature: Creature) -> None:
+        if not self.is_valid(creature):
+            raise InvalidStrategyError(
+                "Battle error, aborting tournament: Invalid Creature"
+                f" '{creature.name}' for this aggressive strategy")
+        assert isinstance(creature, HealCapability)
+        print(creature.describe())
+        print(creature.attack())
+        print(creature.heal())
