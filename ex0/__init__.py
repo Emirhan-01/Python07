52672387ff7f42bfd08db262164b061b482eaeac
Emirhan Bostan <embostan@k2m12s03.42kocaeli.com.tr>
2026-09-11 17:45:25 +0300

    import'lar düzenlendi

--- ex0/__init__.py
+++ ex0/__init__.py
@@ -1,4 +1,5 @@
 from ex0.factory import CreatureFactory, FlameFactory, AquaFactory
+from ex0.creature import Creature
 
 
-__all__ = ["CreatureFactory", "FlameFactory", "AquaFactory"]
+__all__ = ["CreatureFactory", "FlameFactory", "AquaFactory", "Creature"]
--- ex2/strategies.py
+++ ex2/strategies.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
-from ex0.creature import Creature
-from ex1.capability import HealCapability, TransformCapability
+from ex0 import Creature
+from ex1 import HealCapability, TransformCapability
 
 
 class InvalidStrategyError(Exception):
